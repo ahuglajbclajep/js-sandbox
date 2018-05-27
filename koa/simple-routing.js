ca8cd1fb087403ac98a9954b34a6653f635d0810
ahuglajbclajep <ahuglajbclajep@gmail.com>
2018-05-27 17:02:24 +0900

    koa simple-routing

---
+++ koa/simple-routing.js
@@ -0,0 +1,26 @@
+/*
+ * implementation of routing like [koa-route](https://github.com/koajs/route).
+ */
+
+const Koa = require('koa');
+const app = new Koa();
+
+function routing(path, fn) {
+  return async (ctx, next) => {
+    if (path == ctx.path) {
+      fn(ctx);
+    } else {
+      next();
+    }
+  };
+}
+
+app.use(routing('/koa', ctx => {
+  ctx.body = 'Hello, koa';
+}));
+
+app.use(routing('/world', ctx => {
+  ctx.body = 'Hello, world';
+}));
+
+app.listen(3000);
