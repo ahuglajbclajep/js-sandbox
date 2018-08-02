c260a581b5fa064950887b133a145bf46d64d37a
ahuglajbclajep <ahuglajbclajep@gmail.com>
2018-08-03 01:34:12 +0900

    format prettier

--- node/https-promise.js
+++ node/https-promise.js
@@ -2,21 +2,23 @@
  * wrap https.get with Promise
  */
 
-const https = require('https');
+const https = require("https");
 
-function getURL(url) {
+function get(url) {
   return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      if (res.statusCode === 200) {
-        res.setEncoding('utf8');
-        res.on('data', chunk => resolve(chunk));
-      } else {
-        reject(res.statusCode);
-      }
-    }).on('error', err => reject(err.message));
+    https
+      .get(url, res => {
+        if (res.statusCode === 200) {
+          res.setEncoding("utf8");
+          res.on("data", chunk => resolve(chunk));
+        } else {
+          reject(res.statusCode);
+        }
+      })
+      .on("error", err => reject(err.message));
   });
 }
 
-getURL('https://example.com')
+get("https://example.com")
   .then(console.log)
   .catch(console.log);
